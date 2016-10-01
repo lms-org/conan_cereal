73337c221012f8a74993d00b3b5b23774f4fec37
phibedy <philipp.schmette@gmx.de>
2016-10-02 00:47:10 +0200

    init

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class CerealConan(ConanFile):
+    name = "cereal"
+    version = "1.2-0"
+    license = "See: https://github.com/USCiLab/cereal/blob/master/LICENSE"
+    url = "https://github.com/TimSimpson/conan-cereal"
+
+    def source(self):
+       self.run("git clone https://github.com/USCiLab/cereal.git")
+       self.run("cd cereal && git checkout 42a45b6e15fcbd1a3d65b033f5d4d0b2ef6c023d")
+
+    def package(self):
+        self.copy("*", dst="include", src="cereal/include")
+        self.copy("*", dst="include", src="include")
---
+++ include/cereal/cerealizable.h
@@ -0,0 +1,16 @@
+#ifndef CEREAL_CEREALIZABLE_H
+#define CEREAL_CEREALIZABLE_H
+
+#include "lms/serializable.h"
+#include "cereal/archives/portable_binary.hpp"
+
+#define CEREAL_SERIALIZATION() \
+virtual void lmsSerialize(std::ostream &os) const override { \
+    cereal::PortableBinaryOutputArchive oarchive(os); \
+    oarchive(*this); \
+} \
+virtual void lmsDeserialize(std::istream &is) override { \
+    cereal::PortableBinaryInputArchive iarchive(is); \
+    iarchive(*this); \
+}
+
