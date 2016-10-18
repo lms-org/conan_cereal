f1e16ce93c30485b8ae3134e1c00d428f02da730
phibedy <philipp.schmette@gmx.de>
2016-10-18 12:44:14 +0200

    suppressed warning

--- include/cereal/cerealizable.h
+++ include/cereal/cerealizable.h
@@ -6,10 +6,12 @@
 
 #define CEREAL_SERIALIZATION() \
 virtual void lmsSerialize(std::ostream &os,Type type = Type::BINARY) const override { \
+    (void)type;\
     cereal::PortableBinaryOutputArchive oarchive(os); \
     oarchive(*this); \
 } \
 virtual void lmsDeserialize(std::istream &is,Type type = Type::BINARY) override { \
+    (void)type;\
     cereal::PortableBinaryInputArchive iarchive(is); \
     iarchive(*this); \
 }
